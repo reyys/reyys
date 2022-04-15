5ae2a68ccea078af4aa77025c70b425d20e26384
reyys <reyyy0207@gmail.com>
2022-04-15 22:58:44 +0700

    update icon

--- src/components/renderIcon.js
+++ src/components/renderIcon.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RenderIcon = (props) => {
     return (
-        <div className="flex flex-col items-center justify-center p-5 shadow-md shadow-black w-[200px] bg-black hover:scale-110 cursor-pointer duration-300">
+        <div className="flex flex-col items-center justify-center p-5 shadow-md shadow-black w-full max-w-[200px] bg-black hover:scale-110 cursor-pointer duration-300">
             <img src={props.image} className="max-w-[100px] w-full" alt="" />
             <p className="mt-5 font-bold text-2xl text-white">{props.name}</p>
         </div>
